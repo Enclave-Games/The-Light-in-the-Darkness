3fbe4563eac0a3d8135cb09c2db1c9db9cba7733
ArtemGord1728 <artemseraf01@mail.ru>
2019-11-15 10:23:46 +0300

    added files for ai

---
+++ Assets/Scripts/AIPack/EnemyBot/EnemyNavigator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyNavigator : GeneralNavigator
+{
+    public Transform characterTarget;
+    private float speed = 2f;
+
+    public void MoveToTarget(Animator animator, Transform currentEnemy)
+    {
+        characterTarget.transform.position = Vector2.MoveTowards(
+            currentEnemy.transform.position,
+            characterTarget.position,
+            Time.deltaTime * speed);
+        animator.Play("some animations");
+    }
+
+    public void MoveToNoise()
+    {
+
+    }
+
+    public void MoveToSmell()
+    {
+
+    }
+
+    public void ActivateBrain()
+    {
+        
+    }
+
+    public override void MoveToLeft()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void MoveToRight()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/AIPack/GeneralBehaviour/GeneralNavigator.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GeneralNavigator
+{
+    public abstract void MoveToRight();
+    public abstract void MoveToLeft();
+}
